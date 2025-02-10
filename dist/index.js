b349a26ef0f2d5aaa5fd35b5f25476372b77f589
lionhard83 <carloleonardi83@gmail.com>
2025-02-10 16:28:05 +0100

    remove node_modules

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-try {
-    const response1 = await axios.get("https://rickandmortyapi.com/api/character/1");
-    const response2 = axios.get(response1.data.episode[0]);
-    console.log((await response2).data);
-}
-catch (err) {
-    console.log(err);
-}
